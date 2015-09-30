32b808f9af5f81a2c522afd5fde2d7b93ec2c8e2
jessicashannon <jessdahling@gmail.com>
2015-09-30 15:28:15 -0400

    create hidden form to pass parent params to response

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150930190406) do
+ActiveRecord::Schema.define(version: 20150930190750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,11 @@ ActiveRecord::Schema.define(version: 20150930190406) do
   create_table "topics", force: :cascade do |t|
     t.integer  "concept_id"
     t.integer  "prompt_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
