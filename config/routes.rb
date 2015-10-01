3c61670ba41b46dd6a3c21dc194ed5656e9746f7
jessicashannon <jessdahling@gmail.com>
2015-10-01 12:41:31 -0400

    welcome route is singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "welcomes#index"
+  root to: "welcome#index"
   resources :characters, :concepts, :prompts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
