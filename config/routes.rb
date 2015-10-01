e27a665876ff5a0006aba8eecf7f2882770e1357
jessicashannon <jessdahling@gmail.com>
2015-10-01 18:39:04 -0400

    fix routes so log out and log out works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'welcomes#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :characters, :concepts
