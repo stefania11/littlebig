eb8448ad972db5af5694e1393e731b8bff63c23e
stefania11 <druga.stefania@gmail.com>
2015-10-01 12:05:12 -0400

    adding signup form for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_path, notice: "Thank you for signing up!"
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
 end
