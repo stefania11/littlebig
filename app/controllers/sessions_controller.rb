04d90fdb3ae43593511814593099e86929602863
stefania11 <druga.stefania@gmail.com>
2015-10-01 23:05:22 -0400

    fixing template spacing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to prompts_path, notice: "You have succesfully signed in"
     else
-      redirect_to root_path, alert: "Your credentials were incorrect, try again"
+      redirect_to root_path, alert: "Email or password is invalid"
     end
   end
 
