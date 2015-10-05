861b038b50d219e9da7ec20d21a4d013b6408db8
rolandobrown <rolando.brown@gmail.com>
2015-10-05 15:03:04 -0400

    require user avatar on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }
+
+  has_attachment :avatar, accept: [:jpg, :png, :gif]
+  validates :avatar, presence: true
 end
