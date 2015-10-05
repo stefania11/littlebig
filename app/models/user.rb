8b44f78b8d4468c99d87ee9471781d083966b13d
rolandobrown <rolando.brown@gmail.com>
2015-10-05 15:02:12 -0400

    include needed cloudinary and attachinary javascript and jquery files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6}
+  validates :password, length: { minimum: 6 }
 end
