01f360a47de5b65204425390283bce90ee39bae5
jessicashannon <jessdahling@gmail.com>
2015-10-05 16:41:38 -0400

    all flash all the time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,22 @@ class UsersController < ApplicationController
       redirect_to prompts_path, notice: "You have created yo' self!"
       session[:user_id] = @user.id
     else
+      if @user.errors.messages == {:password=>["can't be blank"]}
+        flash[:notice] = "Please enter a password."
+      elsif @user.errors.messages == {:email=>["has already been taken"]}
+        flash[:notice] = "That email has already been taken."
+      elsif @user.errors.messages == {:password_confirmation=>["doesn't match Password"]}
+        flash[:notice] = "Password does not match."
+      elsif @user.errors.messages == {:password=>["is too short (minimum is 6 characters)"]}
+        flash[:notice] = "Passwords must be at least 6 characters long."
+      else
+        flash[:notice] = @user.errors.messages
+      end
       render 'new'
     end
   end
+
+
 
   private
 
