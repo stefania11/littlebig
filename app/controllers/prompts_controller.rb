452517e40ff2a50d705efd8933aa66482e1d513d
stefania11 <druga.stefania@gmail.com>
2015-10-05 16:40:18 -0400

    rendering new prompts on the same page

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -17,20 +17,37 @@ class PromptsController < ApplicationController
     @concept_id = params[:concept_id]
     @character_id = params[:character_id]
     @prompt = Prompt.new
+    respond_to do |format|
+      format.html { redirect_to @prompt }
+      format.js
+    end
   end
 
   # POST /prompts
   def create
     @prompt = Prompt.new(prompt_params)
 
-    render :new, notice: 'Nope.' unless @prompt.save
-
-    if @prompt.source
-      redirect_to @prompt.source, notice: 'Prompt was successfully created.'
-    elsif prompt_params[:concept_id]
-      redirect_to @prompt.concepts.last, notice: 'Prompt was successfully created.'
-    else
-      redirect_to @prompt, notice: 'Prompt was successfully created.'
+    respond_to do |format|
+      format.html {
+        if @prompt.save
+          if @prompt.source
+            redirect_to @prompt.source, notice: 'Prompt was successfully created.'
+          elsif prompt_params[:concept_id]
+            redirect_to @prompt.concepts.last, notice: 'Prompt was successfully created.'
+          else
+            redirect_to @prompt, notice: 'Prompt was successfully created.'
+          end
+        else
+          render :new, notice: 'Nope.'
+        end
+      }
+      format.js {
+        if @prompt.save
+          render :create
+        else
+          render :new
+        end
+      }
     end
   end
 
