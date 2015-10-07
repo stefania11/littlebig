a0a725c5adae2b5e7f68da3864ef7a0266c9ac49
jessicashannon <jessdahling@gmail.com>
2015-10-07 11:07:14 -0400

    add bio to users and make it editable in views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007144244) do
+ActiveRecord::Schema.define(version: 20151007150343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151007144244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.text     "bio"
   end
 
   add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
