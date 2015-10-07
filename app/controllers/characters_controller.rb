bc2ed03d132dc0f9ea34b29b45db0dbd64173414
Drew Price <drewprice@users.noreply.github.com>
2015-10-06 21:39:57 -0400

    edit

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,14 +3,11 @@ class CharactersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /characters
-  # GET /characters.json
   def index
     @characters = Character.all
-    @character = Character.new
   end
 
   # GET /characters/1
-  # GET /characters/1.json
   def show
   end
 
@@ -29,7 +26,6 @@ class CharactersController < ApplicationController
   end
 
   # POST /characters
-  # POST /characters.json
   def create
     @character = Character.new(character_params)
 
@@ -45,36 +41,29 @@ class CharactersController < ApplicationController
   end
 
   # PATCH/PUT /characters/1
-  # PATCH/PUT /characters/1.json
   def update
     respond_to do |format|
       if @character.update(character_params)
         format.html { redirect_to @character, notice: 'Character was successfully updated.' }
-        format.json { render :show, status: :ok, location: @character }
       else
         format.html { render :edit }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /characters/1
-  # DELETE /characters/1.json
   def destroy
     @character.destroy
     respond_to do |format|
       format.html { redirect_to characters_url, notice: 'Character was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_character
       @character = Character.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def character_params
       params.require(:character).permit(:name)
     end
