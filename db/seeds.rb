941faf658a10eceff72f21c9d4b94a3d8659ceb7
Drew Price <drewprice@users.noreply.github.com>
2015-10-09 13:28:28 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,31 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create users
-50.times do
+75.times do
   User.create(email: Faker::Internet.email, username: Faker::Internet.user_name, password: Faker::Internet.password, bio: Faker::Company.catch_phrase)
 end
 
 # Create prompts from users
 100.times do
-  User.find(rand(1..50)).prompts.create(body: Faker::Hacker.say_something_smart, concept_list: Bazaar.object, character_list: Faker::Name.name)
+  User.find(rand(1..75)).prompts.create(body: Faker::Hacker.say_something_smart, concept_list: Bazaar.object, character_list: Faker::Name.name)
 end
 
 # Add overviews & bios
 1.upto(100) do |n|
-  Concept.find(n).overview = Faker::Company.bs
-  Character.find(n).bio = Faker::Company.catch_phrase
+  concept = Concept.find(n)
+  concept.overview = Faker::Company.bs
+  concept.save
+
+  character = Character.find(n)
+  character.bio = Faker::Company.catch_phrase
+  character.save
 end
 
 # Respond to prompts
 200.times do |n|
   rand_id = rand(1..(100 + n))
   prompt = Prompt.find(rand_id)
-  User.find(rand(1..50)).prompts.create(body: Faker::Lorem.sentence, source_id: rand_id)
+  new_prompt = User.find(rand(1..75)).prompts.create(body: Faker::Hacker.say_something_smart, source_id: rand_id)
+  prompt.responses << new_prompt
   prompt.save
 end
