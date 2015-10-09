1b8fc2ec331710fae0c040172612603f12da6f12
rolandobrown <rolando.brown@gmail.com>
2015-10-08 22:09:24 -0400

    add event listeners to change toggle image text on prompt show page

--- app/assets/javascripts/prompts.js
+++ app/assets/javascripts/prompts.js
@@ -1,3 +1,12 @@
 $(function() {
-  $('.attachinary-input').attachinary()
+  $('.attachinary-input').attachinary();
+
+  $("#accordion").on("hide.bs.collapse", function(){
+    $(".toggle").html('Close image <span class="fa fa-camera-retro"></span>');
+  });
+
+  $("#accordion").on("show.bs.collapse", function(){
+    $(".toggle").html('Open image <span class="fa fa-camera-retro"></span>');
+  });
+
 });
