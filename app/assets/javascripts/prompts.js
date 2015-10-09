0c8a5be45d4f44cd219913531d31e0c5243937c4
Drew Price <drewprice@users.noreply.github.com>
2015-10-09 10:14:34 -0400

    Working towards a toggle...

--- app/assets/javascripts/prompts.js
+++ app/assets/javascripts/prompts.js
@@ -1,12 +1,3 @@
 $(function() {
   $('.attachinary-input').attachinary();
-
-  $("#accordion").on("hide.bs.collapse", function(){
-    $(".toggle").html('Close image <span class="fa fa-camera-retro"></span>');
-  });
-
-  $("#accordion").on("show.bs.collapse", function(){
-    $(".toggle").html('Open image <span class="fa fa-camera-retro"></span>');
-  });
-
 });
