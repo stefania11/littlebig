3d1c6fadb8bee3a046f7046bb06da904fcd88230
stefania11 <druga.stefania@gmail.com>
2015-10-09 12:15:57 -0400

    remove votes 2

---
+++ db/schema.rb
@@ -0,0 +1,116 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151008183752) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attachinary_files", force: :cascade do |t|
+    t.integer  "attachinariable_id"
+    t.string   "attachinariable_type"
+    t.string   "scope"
+    t.string   "public_id"
+    t.string   "version"
+    t.integer  "width"
+    t.integer  "height"
+    t.string   "format"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "attachinary_files", ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
+
+  create_table "characters", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.text     "bio"
+  end
+
+  add_index "characters", ["user_id"], name: "index_characters_on_user_id", using: :btree
+
+  create_table "collections", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "prompt_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "collections", ["prompt_id"], name: "index_collections_on_prompt_id", using: :btree
+  add_index "collections", ["user_id"], name: "index_collections_on_user_id", using: :btree
+
+  create_table "concepts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.text     "overview"
+  end
+
+  add_index "concepts", ["user_id"], name: "index_concepts_on_user_id", using: :btree
+
+  create_table "prompts", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "source_id"
+    t.integer  "user_id"
+  end
+
+  add_index "prompts", ["user_id"], name: "index_prompts_on_user_id", using: :btree
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "character_id"
+    t.integer  "prompt_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "relationships", ["character_id"], name: "index_relationships_on_character_id", using: :btree
+  add_index "relationships", ["prompt_id"], name: "index_relationships_on_prompt_id", using: :btree
+
+  create_table "topics", force: :cascade do |t|
+    t.integer  "concept_id"
+    t.integer  "prompt_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "topics", ["concept_id"], name: "index_topics_on_concept_id", using: :btree
+  add_index "topics", ["prompt_id"], name: "index_topics_on_prompt_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "username"
+    t.string   "bio"
+  end
+
+  create_table "welcomes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "characters", "users"
+  add_foreign_key "collections", "prompts"
+  add_foreign_key "collections", "users"
+  add_foreign_key "concepts", "users"
+  add_foreign_key "prompts", "users"
+  add_foreign_key "relationships", "characters"
+  add_foreign_key "relationships", "prompts"
+end
